e47e931edec5681e5b7b15e328b1b9a13ce636b2
Berkan Ankal <berkan.ankal@outlook.com>
2023-01-01 16:16:52 +0300

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BellAlertIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+          alt="Netflix Logo"
+        />
+
+        <ul className="hidden md:flex space-x-4">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden sm:inline h-6 w-6 " />
+        <p className="hidden lg:inline">Kids</p>
+        <BellAlertIcon className="h-6 w-6" />
+        <Link href="/account">
+          <Image
+            src="/assets/images/netflix-avatar.png"
+            alt="Account"
+            className="cursor-pointer rounded"
+            width={30}
+            height={30}
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
