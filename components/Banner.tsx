b37ea2f62b6d1174fce58f5fb47871f64202c4cb
Berkan Ankal <berkan.ankal@outlook.com>
2023-01-05 20:44:41 +0300

    add row component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -32,7 +32,7 @@ const Banner = ({ netflixOriginals }: Props) => {
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs drop-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
         {movie?.overview}
       </p>
       <div className="flex space-x-3">
