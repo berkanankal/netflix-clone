88c90be2edeff93a232abcb7b5581731e457ec6d
Berkan Ankal <berkan.ankal@outlook.com>
2023-01-08 16:25:27 +0300

    create and design auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,113 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
+
+const Auth = () => {
+  const [login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data);
+    // if (login) {
+    //   await signIn(data.email, data.password);
+    // } else {
+    //   await signUp(data.email, data.password);
+    // }
+  };
+
+  return (
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="/assets/images/netflix-background.jpg"
+        fill
+        className="-z-10 !hidden opacity-60 sm:!inline object-cover"
+        alt="Netflix Background"
+      />
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        width={100}
+        height={100}
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        alt="Netflix Logo"
+      />
+
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className={`input ${
+                errors.email && "border-b-2 border-orange-500"
+              }`}
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
+          </label>
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              {...register("password", {
+                required: true,
+                minLength: 4,
+                maxLength: 60,
+              })}
+              placeholder="Password"
+              className={`input ${
+                errors.password && "border-b-2 border-orange-500"
+              }`}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
+          </label>
+        </div>
+        <button
+          className="w-full rounded bg-[#E50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+          type="submit"
+        >
+          Sign In
+        </button>
+        <div className="text-[gray]">
+          New to Netflix?{" "}
+          <button
+            className="cursor-pointer text-white hover:underline"
+            onClick={() => setLogin(false)}
+            type="submit"
+          >
+            Sign up now
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
